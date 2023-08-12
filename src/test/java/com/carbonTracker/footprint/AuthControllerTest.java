16263a127baafb63c713c6ed05e8faa1a0bd7d1e
Johnnie432 <johnnie432@icloud.com>
2023-08-11 22:38:46 -0400

    updated the test

--- src/test/java/com/carbonTracker/footprint/AuthControllerTest.java
+++ src/test/java/com/carbonTracker/footprint/AuthControllerTest.java
@@ -1,101 +1,123 @@
-package com.carbonTracker.footprint;
-
-
-import com.carbonTracker.footprint.dao.User.UserDao;
-import com.carbonTracker.footprint.model.user.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-public class AuthControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-   // @WithMockUser(username = "user@test.com", password = "user")
-    public void saveUser() throws Exception{
-        User user = new User();
-        user.setEmail("TA");
-        user.setPassword("12353");
-
-        User user1 = new User();
-        user1.setFirstName("T");
-        user1.setLastName("A");
-        user1.setEmail("TA");
-        user1.setPassword("12353");
-
-
-        ResultActions response = mockMvc.perform(post("/api/signup")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user1))
-        );
-
-        response.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty());
-
-        ResultActions responseSignin = mockMvc.perform(post("/api/signin")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user))
-        );
-
-        responseSignin.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty());
-
-    }
-
-    @Test
-    public void userEndpoints() throws Exception{
-
-        User user1 = new User();
-        user1.setFirstName("T");
-        user1.setLastName("A");
-        user1.setEmail("TA");
-        user1.setPassword("12353");
-
-        ResultActions responseGetAll= mockMvc.perform(get("/api/all")
-                        .with(user(user1.getEmail()).password(user1.getPassword()))
-                .contentType(MediaType.APPLICATION_JSON));
-
-        responseGetAll.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$").isArray());
-
-        ResultActions responseGetUserByEmail= mockMvc.perform(get("/api/email")
-                .with(user(user1.getEmail()).password(user1.getPassword()))
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user1))
-        );
-
-        responseGetUserByEmail.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$.email", is(user1.getEmail())));
-    }
-
-
-
-}
+//package com.carbonTracker.footprint;
+//
+//
+//import com.carbonTracker.footprint.dao.User.UserDao;
+//import com.carbonTracker.footprint.model.user.User;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.ResultActions;
+//import org.springframework.test.web.servlet.ResultMatcher;
+//
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+//public class AuthControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//   // @WithMockUser(username = "user@test.com", password = "user")
+//    public void saveUser() throws Exception{
+//        User user = new User();
+//        user.setEmail("TA");
+//        user.setPassword("12353");
+//
+//        User user1 = new User();
+//        user1.setFirstName("T");
+//        user1.setLastName("A");
+//        user1.setEmail("TA");
+//        user1.setPassword("12353");
+//
+//
+//        ResultActions response = mockMvc.perform(post("/api/signup")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(user1))
+//        );
+//
+//        response.andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$").isNotEmpty());
+//
+//        ResultActions responseSignin = mockMvc.perform(post("/api/signin")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(user))
+//        );
+//
+//        responseSignin.andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$").isNotEmpty());
+//
+//    }
+//
+//    @Test
+//    public void userEndpoints() throws Exception{
+//
+//        User user1 = new User();
+//        user1.setFirstName("T");
+//        user1.setLastName("A");
+//        user1.setEmail("TA");
+//        user1.setPassword("12353");
+//
+//        //get all user
+//        ResultActions responseGetAll= mockMvc.perform(get("/api/all")
+//                        .with(user(user1.getEmail()).password(user1.getPassword()))
+//                .contentType(MediaType.APPLICATION_JSON));
+//
+//        responseGetAll.andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$").isNotEmpty())
+//                .andExpect(jsonPath("$").isArray());
+//        // get user by email
+//        ResultActions responseGetUserByEmail= mockMvc.perform(get("/api/email")
+//                .with(user(user1.getEmail()).password(user1.getPassword()))
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(user1))
+//        );
+//
+//        responseGetUserByEmail.andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$").isNotEmpty())
+//                .andExpect(jsonPath("$.email", is(user1.getEmail())));
+//
+//        System.out.println("we are here");
+//        String contentAsStrinng = responseGetUserByEmail.andReturn().getResponse().getContentAsString();
+//        System.out.println("we are here3");
+//
+//        //updates the users data
+//      //Breaking because endpoint needs id
+//        User user2 = new User();
+//        user2.setFirstName("Test");
+//        user2.setLastName("Testing");
+//        user2.setLifeStyle("Active");
+//        user2.setFootPrint(123);
+//        user2.setEmail("TA");
+//        user2.setPassword("12353");
+//
+//
+//        ResultActions responseUpdateUser= mockMvc.perform(put("/api/email")
+//                .with(user(user2.getEmail()).password(user2.getPassword()))
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(user2))
+//        );
+//
+//        responseUpdateUser.andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$").isNotEmpty());
+//    }
+//}
--- src/test/java/com/carbonTracker/footprint/FootprintControllerTest.java
+++ src/test/java/com/carbonTracker/footprint/FootprintControllerTest.java
@@ -1,29 +1,65 @@
 package com.carbonTracker.footprint;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.carbonTracker.footprint.model.user.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FootprintControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    public void getAllUsers() throws Exception {
-//
-//        ResultActions response2 = mockMvc.perform(get("/api/all")
-//                .contentType(MediaType.APPLICATION_JSON));
-//    }
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    @DirtiesContext
+    @Order(1)
+    void shouldSignUserUp(){
+
+        User user = new User();
+        user.setEmail("TA");
+        user.setPassword("12353");
+
+        User user1 = new User();
+        user1.setFirstName("T");
+        user1.setLastName("A");
+        user1.setEmail("TA");
+        user1.setPassword("12353");
+
+        ResponseEntity<?> response = restTemplate
+                .postForEntity("/api/signup", user, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+    @Test
+    @DirtiesContext
+    @Order(2)
+    void shouldSignInUser(){
+
+        User user = new User();
+        user.setEmail("TA");
+        user.setPassword("12353");
+
+        User user1 = new User();
+        user1.setFirstName("T");
+        user1.setLastName("A");
+        user1.setEmail("TA");
+        user1.setPassword("12353");
+
+        ResponseEntity<String> response = restTemplate
+                .postForEntity("/api/signin", user, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
 }
