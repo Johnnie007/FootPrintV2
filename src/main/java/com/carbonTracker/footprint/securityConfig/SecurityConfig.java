d8341b8fe51e698564675fc8d4c6ee42b9240eef
Johnnie432 <johnnie432@icloud.com>
2023-09-02 20:03:43 -0400

    update Security config

--- src/main/java/com/carbonTracker/footprint/securityConfig/SecurityConfig.java
+++ src/main/java/com/carbonTracker/footprint/securityConfig/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
@@ -45,8 +50,20 @@ public class SecurityConfig {
                                 .requestMatchers("api/recommendations").permitAll()
                                 .requestMatchers("api/footprint/**").authenticated()
                                 .anyRequest().authenticated()
-                        ).httpBasic(Customizer.withDefaults());
+                        ).httpBasic(Customizer.withDefaults()).cors().configurationSource(corsConfigurationSource());
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
